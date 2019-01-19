9ad3ac403ad0626cac3368e91980ba06e27680d4
robobooga <robobooga@gmail.com>
2019-01-19 17:39:40 +0800

    React: Some uninportant stuff

--- ReactApps/test-app/src/App.js
+++ ReactApps/test-app/src/App.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import Todo from './components/Todo'
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    todo: [
+      {
+        id: 1,
+        title: 'Buy something',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Buy another thing',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Third status',
+        completed: false
+      },
+    ]
+  }
+
   render() {
+    console.log(this.state.todo)
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Herro <code>world</code> saved.
-          </p>
-        </header>
+        <Todo />
       </div>
     );
   }
---
+++ ReactApps/test-app/src/components/Todo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Todo extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Inner component</h1>
+
+      </div>
+    );
+  }
+}
+
+export default Todo;
